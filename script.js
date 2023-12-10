1f4d6cfbb28a9e70aaeb2397f17298cc5543418c
Maisha Maimuna <66841427+maishapriyota@users.noreply.github.com>
2023-12-10 21:52:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+        let menuIcon = document.querySelector('.menuIcon');
+        let nav = document.querySelector('.overlay-menu');
+
+        menuIcon.addEventListener('click', () => {
+            if (nav.style.transform != 'translateX(0%)') {
+                nav.style.transform = 'translateX(0%)';
+                nav.style.transition = 'transform 0.2s ease-out';
+            } else { 
+                nav.style.transform = 'translateX(-100%)';
+                nav.style.transition = 'transform 0.2s ease-out';
+            }
+        });
+
+
+        // Toggle Menu Icon ========================================
+        let toggleIcon = document.querySelector('.menuIcon');
+
+        toggleIcon.addEventListener('click', () => {
+            if (toggleIcon.className != 'menuIcon toggle') {
+                toggleIcon.className += ' toggle';
+            } else {
+                toggleIcon.className = 'menuIcon';
+            }
+        });
+		
+let width = window.innerWidth,
+    height = window.innerHeight,
+    scene,
+    camera,
+    renderer,
+    lights = [];
+    
+import { sculptToMinimalRenderer } from 'https://unpkg.com/shader-park-core/dist/shader-park-core.esm.js';
+
